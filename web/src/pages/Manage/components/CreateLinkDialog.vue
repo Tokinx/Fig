fdc0f91a3ea80b1f94dc66ebee5af39f686598e3
Jeff Gu <idevs@foxmail.com>
2026-04-03 22:48:16 +0800

    feat(style): 创建和编辑窗口UI调整

--- web/src/pages/Manage/components/CreateLinkDialog.vue
+++ web/src/pages/Manage/components/CreateLinkDialog.vue
@@ -135,142 +135,96 @@ watch(
 
 <template>
   <Dialog :open="state.visible" @update:open="(open) => !open && close(false)">
-    <DialogContent class="max-w-2xl max-h-[90vh] overflow-y-auto shadow-none p-2 border-0 bg-transparent">
-      <form @submit.prevent="handleSubmit" class="space-y-6">
+    <DialogContent class="w-[96%] max-w-2xl max-h-[90vh] overflow-y-auto shadow-lg !rounded-2xl bg-white/60">
+      <DialogHeader class="flex-row relative items-center">
+        <DialogTitle>{{ formData.creation ? t("shortLink.editTitle") : t("shortLink.createTitle") }}</DialogTitle>
+        <Button class="w-5 h-5 p-0 !my-0 ml-auto" variant="ghost" size="icon" @click="close(false)">
+          <i class="icon-[material-symbols--close] h-4 w-4" />
+        </Button>
+      </DialogHeader>
+      <form @submit.prevent="handleSubmit">
         <!-- 基础信息 -->
-        <div class="space-y-6">
+        <div class="space-y-4">
           <!-- 目标链接 -->
           <div class="relative">
-            <Textarea
-              id="url"
-              :key="`url-${locale}`"
-              :model-value="formData.url"
+            <Textarea id="url" :key="`url-${locale}`" :model-value="formData.url"
               @update:model-value="(value) => (formData = { ...formData, url: value })"
-              :placeholder="t('shortLink.urlPlaceholder')"
-              class="resize-none rounded-2xl pb-10"
-              :rows="4"
-              :disabled="internalLoading.create"
-            />
+              :placeholder="t('shortLink.urlPlaceholder')" class="resize-none rounded-2xl pb-10" :rows="4"
+              :disabled="internalLoading.create" />
             <div class="absolute bottom-2 left-2 right-2 flex">
               <div class="flex gap-1 bg-white/60 backdrop-blur rounded-full overflow-auto">
-                <Button
-                  v-for="option in modeList"
-                  :key="option.value"
-                  :value="option.value"
+                <Button v-for="option in modeList" :key="option.value" :value="option.value"
                   @click.prevent="switchMode(option.value)"
                   :variant="formData.mode === option.value ? 'default' : 'outline'"
-                  class="rounded-full text-xs h-7 border-0 shadow-none shrink-0"
-                >
+                  class="rounded-full text-xs h-7 border-0 shadow-none shrink-0">
                   {{ option.label }}
                 </Button>
               </div>
               <div class="flex-1 mx-2"></div>
-              <Button
-                type="submit"
-                :disabled="!isFormValid || internalLoading.create"
-                class="rounded-full h-7 self-end gap-2 shrink-0"
-              >
+              <Button type="submit" :disabled="!isFormValid || internalLoading.create"
+                class="rounded-full h-7 self-end gap-2 shrink-0">
                 <i v-if="internalLoading.create" class="icon-[material-symbols--refresh] animate-spin" />
                 <i v-else class="icon-[material-symbols--flash-on] text-sm" />
                 {{ formData.creation ? t("common.update") : t("common.create") }}
               </Button>
             </div>
           </div>
-
-          <div class="flex">
+          <div class="grid gap-4 grid-cols-1 sm:grid-cols-2">
             <!-- 短链接 -->
-            <div class="flex w-full max-w-80 relative rounded-full border border-input shadow-sm bg-slate-100">
-              <div class="flex items-center h-9 px-3 pr-2 text-sm" :class="formData.creation && 'text-slate-500'">
+            <div class="flex w-full h-9 relative rounded-full border border-input shadow-sm bg-slate-100">
+              <div class="flex items-center h-full px-4 text-sm" :class="formData.creation && 'text-slate-500'">
                 {{ originalUrl }}
               </div>
-              <Input
-                type="text"
-                :key="`slug-${locale}`"
-                :class="[
-                  'rounded-full shadow-none border-0 !bg-white !pl-2 pr-10',
-                  formData.creation && '!opacity-100 text-slate-500',
-                ]"
-                :model-value="formData.slug"
+              <Input type="text" :key="`slug-${locale}`" :class="[
+                '!h-full rounded-full shadow-none border-0 !bg-white -ml-1 !pl-3 pr-10',
+                formData.creation && '!opacity-100 text-slate-500',
+              ]" :model-value="formData.slug"
                 @update:model-value="(value) => (formData = { ...formData, slug: value })"
-                :placeholder="t('shortLink.slugPlaceholder')"
-                :disabled="formData.creation"
-                required
-                pattern="[a-zA-Z0-9_\-.]{1,24}"
-              />
-              <Button
-                type="button"
-                variant="outline"
-                size="icon"
+                :placeholder="t('shortLink.slugPlaceholder')" :disabled="formData.creation" required
+                pattern="[a-zA-Z0-9_\-.]{1,24}" />
+              <Button type="button" variant="outline" size="icon"
                 class="rounded-full absolute top-1/2 right-2 -translate-y-1/2 w-6 h-6 flex-shrink-0 active:scale-95"
-                :disabled="formData.creation"
-                @click.prevent="handleRandomize"
-              >
-                <i
-                  v-if="!internalLoading.randomize"
-                  class="icon-[material-symbols--autorenew-outline-rounded] text-base"
-                />
+                :disabled="formData.creation" @click.prevent="handleRandomize">
+                <i v-if="!internalLoading.randomize"
+                  class="icon-[material-symbols--autorenew-outline-rounded] text-base" />
                 <i v-else class="icon-[material-symbols--progress-activity] animate-spin text-base" />
               </Button>
             </div>
-
-            <div class="flex-1"></div>
-
             <!-- 高级设置 -->
-            <Button variant="text" type="button" class="shrink-0 self-end text-xs rounded-full" @click="switchAdvanced">
-              {{ t("shortLink.advancedSettings") }}
-              <i
-                :class="[
+            <div class="text-center sm:text-right">
+              <Button variant="text" type="button" class="shrink-0 self-end text-xs rounded-full"
+                @click="switchAdvanced">
+                {{ t("shortLink.advancedSettings") }}
+                <i :class="[
                   'icon-[material-symbols--expand-more] transition-transform text-lg -mr-1',
                   advanced && 'rotate-180',
-                ]"
-              />
-            </Button>
+                ]" />
+              </Button>
+            </div>
           </div>
 
-          <div
-            ref="advancedArea"
-            :class="['space-y-4 overflow-hidden transition-all px-2 -mx-2', advanced && 'py-1']"
-            :style="{ height: advanced ? advancedHeight + 'px' : '0' }"
-          >
+          <div ref="advancedArea" :class="['space-y-4 overflow-hidden transition-all', advanced ? 'p-1 -m-1' : '!my-0']"
+            :style="{ height: advanced ? advancedHeight + 'px' : '0' }">
             <div class="grid gap-4 grid-cols-1 sm:grid-cols-2">
               <!-- 名称 -->
               <div class="space-y-2">
-                <Input
-                  id="displayName"
-                  :key="`displayName-${locale}`"
-                  class="rounded-full"
+                <Input id="displayName" :key="`displayName-${locale}`" class="rounded-full"
                   :model-value="formData.displayName"
                   @update:model-value="(value) => (formData = { ...formData, displayName: value })"
-                  :placeholder="t('shortLink.displayNamePlaceholder')"
-                  :disabled="internalLoading.create"
-                />
+                  :placeholder="t('shortLink.displayNamePlaceholder')" :disabled="internalLoading.create" />
               </div>
               <!-- 密码 -->
               <div class="space-y-2">
-                <Input
-                  id="passcode"
-                  :key="`passcode-${locale}`"
-                  class="rounded-full"
-                  :model-value="formData.passcode"
+                <Input id="passcode" :key="`passcode-${locale}`" class="rounded-full" :model-value="formData.passcode"
                   @update:model-value="(value) => (formData = { ...formData, passcode: value })"
-                  :placeholder="t('shortLink.passcodePlaceholder')"
-                  :disabled="internalLoading.create"
-                />
+                  :placeholder="t('shortLink.passcodePlaceholder')" :disabled="internalLoading.create" />
               </div>
             </div>
             <!-- 备注 -->
-            <div class="space-y-2">
-              <Textarea
-                id="notes"
-                :key="`notes-${locale}`"
-                :model-value="formData.notes"
-                @update:model-value="(value) => (formData = { ...formData, notes: value })"
-                :placeholder="t('shortLink.notesPlaceholder')"
-                class="rounded-2xl resize-none"
-                rows="4"
-                :disabled="internalLoading.create"
-              />
-            </div>
+            <Textarea id="notes" :key="`notes-${locale}`" :model-value="formData.notes"
+              @update:model-value="(value) => (formData = { ...formData, notes: value })"
+              :placeholder="t('shortLink.notesPlaceholder')" class="rounded-2xl resize-none" rows="4"
+              :disabled="internalLoading.create" />
           </div>
         </div>
       </form>
